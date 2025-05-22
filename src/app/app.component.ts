e3a7b53dd41cab988853944cec8823909c5f9564
locobuzz_hrushikesh_zarekar <hrushikesh.zarekar@locobuzz.com>
2025-05-22 14:31:15 +0530

    First-angular-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';  
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

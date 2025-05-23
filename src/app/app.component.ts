f07aa0531139203daf0fd9c2a8d92527284eb483
locobuzz_hrushikesh_zarekar <hrushikesh.zarekar@locobuzz.com>
2025-05-23 10:10:27 +0530

    implemented ngif and ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';  
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, AboutComponent, FooterComponent],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'First-app';
+  showSkills = false;
+
+  skills = [
+    'Dot Net Framework',
+    'C#',
+    'Java',
+    'HTML',
+    'CSS',
+    'SQL'
+  ];
+
+  toggleSkills() {
+    this.showSkills = !this.showSkills;
+  }
 }

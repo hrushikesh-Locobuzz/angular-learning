7efea54b619312c811210bb1ea934ff035ed6e15
locobuzz_hrushikesh_zarekar <hrushikesh.zarekar@locobuzz.com>
2025-05-26 12:26:20 +0530

    added session 2 learnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,22 @@ import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, AboutComponent, FooterComponent],
+  imports: [CommonModule, FormsModule, RouterModule, HeaderComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'First-app';
   showSkills = false;
+
+  name = '';
 
   skills = [
     'Dot Net Framework',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent }
+];
